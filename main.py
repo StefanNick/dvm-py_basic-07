ea9db35fee94926900f46d23906407d5e2303d8a
StefanNick <stefanidi.n@gmail.com>
2025-03-28 08:11:50 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ def reply(chat_id, time, bot):
 
 def main():
     load_dotenv()
-    TG_TOKEN = os.getenv("TG_TOKEN")
-    bot = ptbot.Bot(TG_TOKEN)
+    tg_token = os.getenv("TG_TOKEN")
+    bot = ptbot.Bot(tg_token)
     bot.reply_on_message(reply, bot=bot)
     bot.run_bot()
 
